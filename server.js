1d32e5b38eb2c70183cefc884b44a5bace610839
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-01-26 15:53:04 +0100

    set up rootquery and root mutation

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp= require('express-graphql')
-const { buildSchema }= require('graphql')
+const { buildSchema } = require('graphql')
 
 
 const app = express()
 
 app.use(bodyParser.json())
 
+const events = []
+
 const schema = buildSchema(`
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+
   type RootQuery {
-    events: [String!]!
+    events: [Event!]!
   }
 
   type RootMutation {
-    createEvent(name: String) : String
+    createEvent(eventInput: EventInput) : Event
   }
 
   schema {
@@ -25,20 +42,23 @@ const schema = buildSchema(`
 
 app.use('/graphql', graphqlHttp({
   schema,
-  rootValue:{
+  rootValue:  {
     events: () => {
-      return ['cooking', 'sailing', 'coding']
+      return events
     },
     createEvent: (args) => {
-      const eventName = args.name
-      return eventName
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date().toISOString()
+      }
+      events.push(event)
+      return event
     }
   },
   graphiql: true
 }))
-
-// app.get('/', (req, res, next) => {
-//   res.send('Hello world')
-// })
 
 app.listen(3000)
