be6ad32b58a594bee0ad8ad2f9afa7e2aced88d5
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-01-26 13:44:53 +0100

    set up basic graphql schema and resolvers

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp= require('express-graphql')
+const { buildSchema }= require('graphql')
+
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world')
-})
+const schema = buildSchema(`
+  type RootQuery {
+    events: [String!]!
+  }
+
+  type RootMutation {
+    createEvent(name: String) : String
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`);
+
+app.use('/graphql', graphqlHttp({
+  schema,
+  rootValue:{
+    events: () => {
+      return ['cooking', 'sailing', 'coding']
+    },
+    createEvent: (args) => {
+      const eventName = args.name
+      return eventName
+    }
+  },
+  graphiql: true
+}))
+
+// app.get('/', (req, res, next) => {
+//   res.send('Hello world')
+// })
 
 app.listen(3000)
