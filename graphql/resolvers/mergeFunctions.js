37690ad7b6cbba3d36070033f7503a69b531f9aa
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-02-18 14:03:38 +0100

    finished app
    sort ids in get events to match with dataloader

--- graphql/resolvers/mergeFunctions.js
+++ graphql/resolvers/mergeFunctions.js
@@ -36,6 +36,9 @@ const getEvents = async eventIds => {
 
   try {
     const events = await Event.find({_id: {$in: eventIds}})
+    events.sort((a,b) => {
+      return eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+    })
     return events.map(event => {
       return transformEvent(event)
     })
